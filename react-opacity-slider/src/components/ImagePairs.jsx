48ec2b2333d0dd7fc3be84efbd46ffef58f33393
Poitier Stringer <stringerpoitier@gmail.com>
2023-03-05 14:19:09 -0800

    layer images on top of one another

--- react-opacity-slider/src/components/ImagePairs.jsx
+++ react-opacity-slider/src/components/ImagePairs.jsx
@@ -1,23 +1,28 @@
 import OpacityImage from "./OpacityImage";
 
 const imagePairStyles = {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    gap: '8px',
-    margin: '8px',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center',
+  gap: '8px',
+  margin: '8px',
+  position: 'relative', // Set position to relative
 }
 
-const ImagePairs = ({imagePair, opacity}) => {
-    const transformation1 = (x) => (-x + 100) / 100;
-    const transformation2 = (x) => x / 100;
+const ImagePairs = ({ imagePair, opacity }) => {
+  const transformation1 = (x) => (-x + 100) / 100;
+  const transformation2 = (x) => x / 100;
 
-    return ( 
-        <div className="image-pairs" style={imagePairStyles}>
-            <OpacityImage src={imagePair.img1} opacity={transformation1(opacity)}/>
-            <OpacityImage src={imagePair.img2} opacity={transformation2(opacity)}/>
-        </div>
-    );
+  return (
+    <div className="image-pairs" style={imagePairStyles}>
+      <div style={{ position: 'absolute', top: 0, left: 0 }}>
+        <OpacityImage src={imagePair.img1} opacity={transformation1(opacity)} />
+      </div>
+      <div style={{ position: 'absolute', top: 0, left: 0 }}>
+        <OpacityImage src={imagePair.img2} opacity={transformation2(opacity)} />
+      </div>
+    </div>
+  );
 }
- 
+
 export default ImagePairs;
